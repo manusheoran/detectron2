3be6d7c4da6297351c6f46a5f4e1cd7493d1c254
meghalD <meghal.dani.er@gmail.com>
2021-07-27 15:22:39 +0530

    Update rcnn.py

--- detectron2/modeling/meta_arch/rcnn.py
+++ detectron2/modeling/meta_arch/rcnn.py
@@ -616,7 +616,7 @@ class GradientReversal(torch.nn.Module):
 #         return output, None 
 
 class FCOSDiscriminator(nn.Module):
-    def __init__(self, num_convs=2, in_channels=256, grad_reverse_lambda=-1.0, grl_applied_domain='both'):
+    def __init__(self, num_convs=2, in_channels=512, grad_reverse_lambda=-1.0, grl_applied_domain='both'):
         """
         Arguments:
             in_channels (int): number of channels of the input feature
@@ -676,6 +676,8 @@ class FCOSDiscriminator(nn.Module):
         return loss
 
 
+
+    
 @META_ARCH_REGISTRY.register()
 class ProposalNetwork_DA(nn.Module):
     """
@@ -700,6 +702,33 @@ class ProposalNetwork_DA(nn.Module):
         """
         super().__init__()
         self.backbone = backbone
+        
+        #DA FPN Layers :
+        self.dis_P7 = FCOSDiscriminator(
+                num_convs=4,
+                grad_reverse_lambda=0.01,
+                grl_applied_domain="both").to(device)
+ 
+        self.dis_P6 = FCOSDiscriminator(
+                        num_convs=4,
+                        grad_reverse_lambda=0.01,
+                        grl_applied_domain="both").to(device)
+
+        self.dis_P5 = FCOSDiscriminator(
+                        num_convs=4,
+                        grad_reverse_lambda=0.01,
+                        grl_applied_domain="both").to(device)
+
+        self.dis_P4 = FCOSDiscriminator(
+                        num_convs=4,
+                        grad_reverse_lambda=0.01,
+                        grl_applied_domain="both").to(device)
+
+        self.dis_P3 = FCOSDiscriminator(
+                        num_convs=4,
+                        grad_reverse_lambda=0.01,
+                        grl_applied_domain="both").to(device)
+        
         self.proposal_generator = proposal_generator
         self.register_buffer("pixel_mean", torch.tensor(pixel_mean).view(-1, 1, 1), False)
         self.register_buffer("pixel_std", torch.tensor(pixel_std).view(-1, 1, 1), False)
@@ -718,6 +747,83 @@ class ProposalNetwork_DA(nn.Module):
     def device(self):
         return self.pixel_mean.device
 
+    #DA Loss function
+    def losses(self, anchors, pred_logits, gt_labels, pred_anchor_deltas, gt_boxes, res3, res4, res5, domain_target, alpha3, alpha4, alpha5):
+        """
+        Args:
+            anchors (list[Boxes]): a list of #feature level Boxes
+            gt_labels, gt_boxes: see output of :meth:`RetinaNet.label_anchors`.
+                Their shapes are (N, R) and (N, R, 4), respectively, where R is
+                the total number of anchors across levels, i.e. sum(Hi x Wi x Ai)
+            pred_logits, pred_anchor_deltas: both are list[Tensor]. Each element in the
+                list corresponds to one level and has shape (N, Hi * Wi * Ai, K or 4).
+                Where K is the number of classes used in `pred_logits`.
+        Returns:
+            dict[str, Tensor]:
+                mapping from a named loss to a scalar tensor
+                storing the loss. Used during training only. The dict keys are:
+                "loss_cls" and "loss_box_reg"
+        """
+        if domain_target:
+            loss_p7 = self.dis_P7
+            loss_p6 = self.dis_P6
+            loss_p5 = self.dis_P5
+            loss_p4 = self.dis_P4
+            loss_p3 = self.dis_P3
+            return {"loss_p7": loss_p7,"loss_p6": loss_p6,"loss_p5": loss_p5,"loss_p4": loss_p4,"loss_p3": loss_p3}
+            #return {"loss_r3": loss_res3, "loss_r4": loss_res4, "loss_r5": loss_res5}
+        else:
+            loss_p7 = self.dis_P7
+            loss_p6 = self.dis_P6
+            loss_p5 = self.dis_P5
+            loss_p4 = self.dis_P4
+            loss_p3 = self.dis_P3
+            return {"loss_p7": loss_p7,"loss_p6": loss_p6,"loss_p5": loss_p5,"loss_p4": loss_p4,"loss_p3": loss_p3}
+#             loss_res3 = self.discriminatorRes3(res3, domain_target, alpha3)
+#             loss_res4 = self.discriminatorRes4(res4, domain_target, alpha4)
+#             loss_res5 = self.discriminatorRes5(res5, domain_target, alpha5)
+                
+
+        num_images = len(gt_labels)
+        gt_labels = torch.stack(gt_labels)  # (N, R)
+        anchors = type(anchors[0]).cat(anchors).tensor  # (R, 4)
+        gt_anchor_deltas = [self.box2box_transform.get_deltas(anchors, k) for k in gt_boxes]
+        gt_anchor_deltas = torch.stack(gt_anchor_deltas)  # (N, R, 4)
+
+        valid_mask = gt_labels >= 0
+        pos_mask = (gt_labels >= 0) & (gt_labels != self.num_classes)
+        num_pos_anchors = pos_mask.sum().item()
+        get_event_storage().put_scalar("num_pos_anchors", num_pos_anchors / num_images)
+        self.loss_normalizer = self.loss_normalizer_momentum * self.loss_normalizer + (
+            1 - self.loss_normalizer_momentum
+        ) * max(num_pos_anchors, 1)
+
+        # classification and regression loss
+        gt_labels_target = F.one_hot(gt_labels[valid_mask], num_classes=self.num_classes + 1)[
+            :, :-1
+        ]  # no loss for the last (background) class
+        loss_cls = sigmoid_focal_loss_jit(
+            cat(pred_logits, dim=1)[valid_mask],
+            gt_labels_target.to(pred_logits[0].dtype),
+            alpha=self.focal_loss_alpha,
+            gamma=self.focal_loss_gamma,
+            reduction="sum",
+        )
+
+        loss_box_reg = smooth_l1_loss(
+            cat(pred_anchor_deltas, dim=1)[pos_mask],
+            gt_anchor_deltas[pos_mask],
+            beta=self.smooth_l1_loss_beta,
+            reduction="sum",
+        )
+        return {
+            "loss_cls": loss_cls / self.loss_normalizer,
+            "loss_box_reg": loss_box_reg / self.loss_normalizer,
+            "loss_r3": loss_res3,
+            "loss_r4": loss_res4,
+            "loss_r5": loss_res5
+        }
+    
     def forward(self, batched_inputs):
         """
         Args:
