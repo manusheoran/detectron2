200c94449e80b6b5ad63a274990ccf1757c2de7f
meghalD <meghal.dani.er@gmail.com>
2021-07-29 12:43:06 +0530

    Update coco_evaluation.py

--- detectron2/evaluation/coco_evaluation.py
+++ detectron2/evaluation/coco_evaluation.py
@@ -134,7 +134,7 @@ class COCOEvaluator(DatasetEvaluator):
         """
         for input, output in zip(inputs, outputs):
             prediction = {"image_id": input["image_id"]}
-
+            print("\n \n output loop testing", output)
             if "instances" in output:
                 instances = output["instances"].to(self._cpu_device)
                 prediction["instances"] = instances_to_coco_json(instances, input["image_id"])
