04c876abba943339d69dc4064833ca65e418a466
meghalD <meghal.dani.er@gmail.com>
2021-08-10 10:55:00 +0530

    Update __init__.py

--- detectron2/modeling/meta_arch/__init__.py
+++ detectron2/modeling/meta_arch/__init__.py
@@ -6,7 +6,7 @@ from .build import META_ARCH_REGISTRY, build_model  # isort:skip
 from .panoptic_fpn import PanopticFPN
 
 # import all the meta_arch, so they will be registered
-from .rcnn import GeneralizedRCNN, ProposalNetwork, ProposalNetwork1, ProposalNetwork_DA
+from .rcnn import GeneralizedRCNN, ProposalNetwork, ProposalNetwork1, ProposalNetwork_DA, ProposalNetwork_DA_CA
 from .retinanet import RetinaNet
 from .semantic_seg import SEM_SEG_HEADS_REGISTRY, SemanticSegmentor, build_sem_seg_head
 
